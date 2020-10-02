68bc1579b8558234331062efc5baa52fb6e762ac
Mario Zimmermann <mail@zisoft.de>
2020-10-02 08:06:38 +0200

    Update rsync settings

--- aliases.zsh
+++ aliases.zsh
@@ -12,9 +12,9 @@ alias mysql_stop='brew services stop mysql'
 
 alias brewup='brew update; brew upgrade; brew upgrade --cask; brew cleanup; brew doctor'
 
-alias sync_pics='rsync -av --delete ~/Pictures/ /Volumes/Marios/Pictures'
-alias sync_pics_synnas='rsync -av --delete --delete-excluded --exclude-from ~/.oh-my-zsh/custom/rsync_excludes ~/Pictures/ -e ssh mario@synnas:/volume1/Mario/Pictures'
-alias sync_pics_zisoft='rsync -r -t -v --delete --delete-excluded --exclude-from ~/.oh-my-zsh/custom/rsync_excludes ~/Sites/october/storage/app/media/ -e ssh zisoft.de:/www/htdocs/w0107c10/october/storage/app/media/'
+alias sync_pics='rsync -av --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Pictures/ /Volumes/Marios/Pictures'
+alias sync_pics_synnas='rsync -av --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Pictures/ -e ssh mario@synnas:/volume1/Mario/Pictures'
+alias sync_pics_zisoft='rsync -r -t -v --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Sites/october/storage/app/media/ -e ssh zisoft.de:/www/htdocs/w0107c10/october/storage/app/media/'
 
-alias sync_music='rsync -av --delete --delete-excluded --exclude-from ~/.oh-my-zsh/custom/rsync_excludes ~/Music/ /Volumes/Marios/Music'
-alias sync_music_synnas='rsync -av --delete --delete-excluded --exclude-from ~/.oh-my-zsh/custom/rsync_excludes ~/Music/ -e ssh mario@synnas:/volume1/Mario/Music'
+alias sync_music='rsync -av --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Music/ /Volumes/Marios/Music'
+alias sync_music_synnas='rsync -av --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Music/ -e ssh mario@synnas:/volume1/Mario/Music'
