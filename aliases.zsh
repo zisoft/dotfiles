98f434232cf42712b812c5863816d41ce674908f
Mario Zimmermann <mail@zisoft.de>
2020-10-11 13:56:00 +0200

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -2,7 +2,7 @@
 alias la='ls -alh'
 
 alias web='cd ~/Sites/october/themes/zisoft'
-alias ios='cd ~/Software/iOS'
+alias ios='cd ~/Documents/Software/iOS'
 
 alias web_start='brew services run httpd && brew services run mysql'
 alias web_stop='brew services stop httpd && brew services stop mysql'
@@ -12,9 +12,9 @@ alias mysql_stop='brew services stop mysql'
 
 alias brewup='brew update; brew upgrade; brew upgrade --cask; brew cleanup; brew doctor'
 
-alias sync_pics='rsync -av --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Pictures/ /Volumes/Marios/Pictures'
-alias sync_pics_synnas='rsync -av --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Pictures/ -e ssh mario@synnas:/volume1/Mario/Pictures'
-alias sync_pics_zisoft='rsync -r -t -v --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Sites/october/storage/app/media/ -e ssh zisoft.de:/www/htdocs/w0107c10/october/storage/app/media/'
+alias sync_pics='rsync -av --delete --delete-excluded --exclude-from='/Users/mario/Documents/dotfiles/rsync_excludes' ~/Pictures/ /Volumes/Marios/Pictures'
+alias sync_pics_synnas='rsync -av --delete --delete-excluded --exclude-from='/Users/mario/Documents/dotfiles/rsync_excludes' ~/Pictures/ -e ssh mario@synnas:/volume1/Mario/Pictures'
+alias sync_pics_zisoft='rsync -r -t -v --delete --delete-excluded --exclude-from='/Users/mario/Documents/dotfiles/rsync_excludes' ~/Sites/october/storage/app/media/ -e ssh zisoft.de:/www/htdocs/w0107c10/october/storage/app/media/'
 
-alias sync_music='rsync -av --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Music/ /Volumes/Marios/Music'
-alias sync_music_synnas='rsync -av --delete --delete-excluded --exclude-from ~/Documents/dotfiles/rsync_excludes ~/Music/ -e ssh mario@synnas:/volume1/Mario/Music'
+alias sync_music='rsync -av --delete --delete-excluded --exclude-from='/Users/mario/Documents/dotfiles/rsync_excludes' ~/Music/ /Volumes/Marios/Music'
+alias sync_music_synnas='rsync -av --delete --delete-excluded --exclude-from='/Users/mario/Documents/dotfiles/rsync_excludes' ~/Music/ -e ssh mario@synnas:/volume1/Mario/Music'
