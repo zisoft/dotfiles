f11ae61c0f25f6fa06b03d1a964bfcfa41242ca3
Mario Zimmermann <mail@zisoft.de>
2020-12-29 09:16:15 +0100

    Several changes and updates

--- fresh.sh
+++ fresh.sh
@@ -15,15 +15,13 @@ brew tap homebrew/bundle
 brew bundle
 
 # Set default MySQL root password and auth type.
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
-
+#mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/Documents/dotfiles/.zshrc $HOME/.zshrc
 
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/Documents/dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
