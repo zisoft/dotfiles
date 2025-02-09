d6c318da85cb3eeb161ad2ced76b212ea6eba20d
Mario Zimmermann <mail@zisoft.de>
2025-02-09 20:13:38 +0100

    keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -34,6 +34,7 @@ return {
 
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>ls", builtin.buffers, {})
     end,
   },
   {
