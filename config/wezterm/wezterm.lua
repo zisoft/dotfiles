aa736df11e175f674d04cb822e4ab12484524438
Mario Zimmermann <mail@zisoft.de>
2025-03-09 17:31:21 +0100

    wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require("wezterm")
+wezterm.on("gui-startup", function(cmd)
+	-- set startup Window position
+	wezterm.mux.spawn_window(cmd or { position = { x = 1400, y = 200 } })
+end)
+
+-- wezterm config
+local config = wezterm.config_builder()
+
+config.initial_rows = 50
+config.initial_cols = 130
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 13
+
+config.color_scheme = "Catppuccin Frappe"
+
+return config
