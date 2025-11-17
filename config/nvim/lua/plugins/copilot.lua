43bc402a490100104f49e619adfa1ef41aa351c3
Mario Zimmermann <mail@zisoft.de>
2025-11-17 20:04:23 +0100

    fix typos

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
   { src = "https://github.com/CopilotC-Nvim/CopilotChat.nvim" },
-  { src = "https://github.com/vim-lua/plenary.nvim" },
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
 })
 
 require("CopilotChat").setup({
